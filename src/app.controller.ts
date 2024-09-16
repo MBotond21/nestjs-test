fcf795d01d84b395ca5c4b186676ea97f1d11ba2
MBotond21 <m.botond07@gmail.com>
2024-09-16 13:59:01 +0200

    majndem kész

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { quotes } from './quotes';
+import { map } from 'rxjs';
+import { response } from 'express';
 
 @Controller()
 export class AppController {
@@ -12,4 +15,41 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
+
+  @Get('quotes')
+  @Render('quotes')
+  Qoutes() {
+    return {
+      response: quotes
+    };
+  };
+
+  @Get('randomQuote')
+  @Render('randomQuotes')
+  RandQoutes() {
+    return {
+      response: quotes,
+      id: Math.floor(Math.random() * quotes.quotes.length)
+    };
+  };
+
+  @Get('topAuthor')
+  @Render('topAuthor')
+  TopAuthor() {
+
+    var authors = new Map
+
+    for (let i = 0; i < quotes.quotes.length; i++) {
+      if (authors.has(quotes.quotes[i].author)){
+        authors.set(quotes.quotes[i].author, authors.get(quotes.quotes[i].author)+1);
+      }
+      else{
+        authors.set(quotes.quotes[i].author, 1);
+      }
+    }
+
+    return {
+      response: authors
+    };
+  };
 }
